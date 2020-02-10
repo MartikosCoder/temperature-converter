b3d86a609de0bd7fac5db1be6359a1107e27e10e
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2020-02-10 14:23:40 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 
 
-exports.cToF = function (c) {
+exports.cToF = c => (c * 1.8 + 32).toFixed(2);
 
-}
-
-exports.fToC = function (f) {
-
-}
+exports.fToC = f => ((f - 32) * 0.55556).toFixed(2); // 0.55556 stands for 5/9 => made for reducing calculations
